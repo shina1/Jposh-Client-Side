6f8b07fcef1ff33f4d089e3fe54069abf0df7897
shina1 <oswasiu@gmail.com>
2022-03-21 12:38:22 +0100

    ft footer component

--- jposhcollections/src/screens/home-screen/index.js
+++ jposhcollections/src/screens/home-screen/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import About from '../../Components/About'
 import Categories from '../../Components/Categories'
+import Footer from '../../Components/Footer'
 // import Header from '../../Components/Header-component'
 import HeaderRes from '../../Components/Header-component/Header'
+import Newsletter from '../../Components/Newsletter'
 import Products from '../../Components/Product/Products'
 import CarouselSlider from '../../Components/SlidderComponent/Carousel'
 // import Slidder from '../../Components/SlidderComponent/index.js'
@@ -15,6 +17,8 @@ const Home = () => {
         <About />
         <Categories />
         <Products />
+        <Newsletter />
+        <Footer />
     </div>
   )
 }
